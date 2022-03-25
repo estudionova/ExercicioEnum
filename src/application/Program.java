f35eacb0b94f69ace996bd46d7caaff8711b1ca4
Reinaldo Junior <rsljunior@live.com>
2022-03-25 20:34:24 -0300

    Finalização

--- src/application/Program.java
+++ src/application/Program.java
@@ -23,7 +23,7 @@ public class Program {
         System.out.print("Enter department's name: ");
         String departmentName = sc.nextLine();
 
-        System.out.print("Enter worker Data: ");
+        System.out.println("Enter worker Data: ");
 
         System.out.print("Name: ");
         String workerName = sc.nextLine();
@@ -41,7 +41,7 @@ public class Program {
         System.out.println("How many contracts to this worker? ");
         int n = sc.nextInt();
 
-        for (int i = 1; i < n; i++) {
+        for (int i = 1; i <= n; i++) {
             System.out.println("Enter contract #" + i + " data: ");
             System.out.print("Date ( DD/MM/YYYY):  ");
             Date contractDate = sdf.parse(sc.next());
@@ -53,9 +53,19 @@ public class Program {
 
             HourContract contract = new HourContract(contractDate, valuePerHour, hours);
             worker.addContract(contract);
-
-
         }
+
+        System.out.println();
+
+        System.out.println("Enter month and year to calculate income ( MM / YYYY ) : ");
+        String monthAndYear = sc.next();
+        int month = Integer.parseInt( monthAndYear.substring(0, 2));
+        int year = Integer.parseInt(monthAndYear.substring(3));
+
+        System.out.println("Name: " + worker.getName());
+        System.out.println("Departament: "+ worker.getDepartament().getName());
+        System.out.println("Income for " + monthAndYear + ": " + String.format( "%.2f",  worker.income(year, month)));
+
 
 
         sc.close();
